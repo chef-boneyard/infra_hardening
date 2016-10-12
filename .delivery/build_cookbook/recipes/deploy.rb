5849bbf51534bdcc1128876e7b873aa1705dbf1a
yvovandoorn <yvo@chef.io>
2016-10-12 15:52:29 +0100

    Upload deploy recipe

--- .delivery/build_cookbook/recipes/deploy.rb
+++ .delivery/build_cookbook/recipes/deploy.rb
@@ -3,4 +3,31 @@
 # Recipe:: deploy
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
-include_recipe 'delivery-truck::deploy'
+#include_recipe 'delivery-truck::deploy'
+
+search_terms = []
+get_all_project_cookbooks.each do |cookbook|
+  search_terms << "recipes:#{cookbook.name}*"
+end
+
+log "Search_Terms: #{search_terms}"
+
+unless search_terms.empty?
+  search_query = "(#{search_terms.join(' OR ')}) " \
+                 "AND chef_environment:#{delivery_environment} " \
+                 "AND #{deployment_search_query}"
+
+                 log "Search_Query: #{search_query}"
+
+
+  my_nodes = delivery_chef_server_search(:node, search_query)
+
+  log "Nodes: #{my_nodes}"
+
+  my_nodes.map!(&:name)
+
+  delivery_push_job "deploy_#{node['delivery']['change']['project']}" do
+    command 'chef-client'
+    nodes my_nodes
+  end
+end
